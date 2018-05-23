838ba9a8e62240cf8b0477e20feec03652c45fca
Daniel Kats <daniel_kats@symantec.com>
2018-05-23 01:58:34 -0700

    bugfixes

--- js/static-data.js
+++ js/static-data.js
@@ -7,28 +7,48 @@ StaticGameData.Phases = [
     Card.types.UPGRADE
 ];
 
+StaticGameData.aristocratScoringChart = {
+    0: 0,
+    1: 1,
+    2: 3,
+    3: 6,
+    4: 10,
+    5: 15,
+    6: 21,
+    7: 28,
+    8: 36,
+    9: 45,
+    10: 55
+};
+
 /**
  * 
  * @param {number} numAristocrats Number of aristocrats this player owns
+ * @returns {number}
  */
 StaticGameData.scoreAristocrats = function(numAristocrats) {
     numAristocrats = Math.min(numAristocrats, 10);
     return StaticGameData.aristocratScoringChart[numAristocrats];
 };
 
+/**
+ * 
+ * @param {number} phase
+ * @returns {String}
+ */
 StaticGameData.getPhaseName = function(phase) {
     phase = Number(phase);
     switch (phase) {
-        case Card.types.WORKER:
-            return "Worker";
-        case Card.types.BUILDING:
-            return "Building";
-        case Card.types.ARISTOCRAT:
-            return "Aristocrat";
-        case Card.types.UPGRADE:
-            return "Upgrade";
-        default:
-            throw new Error("Unknown phase: " + phase);
+    case Card.types.WORKER:
+        return "Worker";
+    case Card.types.BUILDING:
+        return "Building";
+    case Card.types.ARISTOCRAT:
+        return "Aristocrat";
+    case Card.types.UPGRADE:
+        return "Upgrade";
+    default:
+        throw new Error("Unknown phase: " + phase);
     }
 };
 
